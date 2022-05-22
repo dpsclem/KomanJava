49a5d027c509f70ec137a8109c3c542ea705c2d5
Clément <cledupas@gmail.com>
2022-05-22 17:23:30 +0200

    Added GUIStarter for JAR executable file

---
+++ src/main/java/Launch/GUIStarter.java
@@ -0,0 +1,10 @@
+package Launch;
+
+import com.example.komanjava.HelloApplication;
+
+public class GUIStarter {
+    public static void main(final String[] args) {
+        HelloApplication.main(args);
+    }
+}
+
